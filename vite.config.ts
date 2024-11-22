9be6aeff97bb1f0edf4cf21ca4ba5f5fa51e48d4
Pontus Pohl <pontus.pohl@gmail.com>
2024-11-22 14:19:53 +0100

    fix: Update Vite config base path and remove React plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'kids-sudoku',
-  plugins: [react(), VitePWA({
+  base: '/kids-sudoku',
+  plugins: [VitePWA({
     strategies: 'injectManifest',
     srcDir: 'src',
     filename: 'sw.ts',
