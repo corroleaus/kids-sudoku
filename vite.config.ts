ef13308afc4b7eae0f0026d5467508c0ee10d56e
Pontus Pohl <pontus.pohl@gmail.com>
2024-11-22 14:24:01 +0100

    fix: Update base path in Vite config to include trailing slash

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/kids-sudoku',
+  base: '/kids-sudoku/',
   plugins: [VitePWA({
     strategies: 'injectManifest',
     srcDir: 'src',
