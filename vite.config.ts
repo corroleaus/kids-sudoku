315802cefbfaaeeddfc45d87e9e1d68c128d917e
Pontus Pohl <pontus.pohl@gmail.com>
2024-11-22 14:05:35 +0100

    fix: Update Vite config base path for PWA and project

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'kids-sudoku',
   plugins: [react(), VitePWA({
     strategies: 'injectManifest',
     srcDir: 'src',
-    base: 'kids-sudoku',
     filename: 'sw.ts',
     registerType: 'prompt',
     injectRegister: false,
